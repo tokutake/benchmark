12b04ea34df785e79cf932fd2f569de4eabe76a4
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-26 21:09:06 +0900

    Move zig source to zig project folder

--- loop/zig/src/main.zig
+++ loop/zig/src/main.zig
@@ -1,13 +1,27 @@
 const std = @import("std");
 
-pub fn main() void {
-    const args = std.os.Args.init();
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn();
 
-    // Check if there are command-line arguments
-    if (args.argCount() < 2) {
-        // Print usage message
-        const execName = args.arg0();
-        std.debug.print("Usage: {} <upper limit number>\n", .{execName});
+    var line_buf: [1024]u8 = undefined;
+    const amount = try stdin.read(&line_buf);
+    if (amount == line_buf.len) {
+        try stdout.print("Input too long\n", .{});
         return;
     }
+    const line = std.mem.trimRight(u8, line_buf[0..amount], "\r\n");
+    const input_number = std.fmt.parseUnsigned(i64, line, 10) catch {
+        try stdout.print("Invalid number.\n", .{});
+        return;
+    };
+
+    var sum: i64 = 0;
+    var upper: i64 = input_number;
+    var i: i64 = 0;
+    while (i <= upper) {
+        sum += i;
+        i += 1;
+    }
+    try stdout.print("{}", .{sum});
 }
