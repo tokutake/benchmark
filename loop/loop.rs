14f77ab5c88d0b34d50926978d34e0a910d6ba33
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 08:35:56 +0900

    User inclusive range in Rust

--- loop/loop.rs
+++ loop/loop.rs
@@ -4,7 +4,7 @@ fn main() {
     let upper : i64 = env::args().nth(1).expect("argument does not exist").parse().unwrap();
     let mut sum : i64 = 0;
 
-    for i in 1..upper {
+    for i in 1..=upper {
         sum += i;
     }
     println!("{}", sum);
