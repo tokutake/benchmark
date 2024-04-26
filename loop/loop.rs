9fe99995ab7c4ffd5419e82d1193986e89a99fee
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 08:21:26 +0900

    Add rust program

---
+++ loop/loop.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    let upper : i64 = env::args().nth(1).expect("argument does not exist").parse().unwrap();
+    let mut sum : i64 = 0;
+
+    for i in 1..upper {
+        sum += i;
+    }
+    println!("{}", sum);
+}
