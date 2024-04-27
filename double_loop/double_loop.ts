46c30dca73fce30f4faa43fdc214e47f25ad8890
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 16:04:02 +0900

    Add JavaScript code

---
+++ double_loop/double_loop.ts
@@ -0,0 +1,12 @@
+console.log(Deno.args[0]);
+const MAX : number = parseInt(Deno.args[0]);
+
+let num: number = 0;
+for (let i: number = 0; i < MAX; i++) {
+    for (let j: number = 0; j < MAX; j++) {
+        num += j;
+        num = num ^ 0xfff;
+    }
+}
+
+console.log(num);
