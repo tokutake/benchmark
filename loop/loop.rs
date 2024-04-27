0d083939ad2f4131a71e08bbd074961445ee81c5
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 15:19:04 +0900

    Replace while with range loop in Rust code

--- loop/loop.rs
+++ loop/loop.rs
@@ -4,8 +4,10 @@ fn main() {
     let upper : i64 = env::args().nth(1).expect("argument does not exist").parse().unwrap();
     let mut sum : i64 = 0;
 
-    for i in 1..=upper {
+    let mut i : i64 = 0;
+    while i <= upper {
         sum += i;
+        i += 1;
     }
     println!("{}", sum);
 }
