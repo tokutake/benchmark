8161eac866f88e8b71dfcce97eb94fff102951be
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 16:03:36 +0900

    Use j in the inner loop

--- double_loop/double_loop.cpp
+++ double_loop/double_loop.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     for (int32_t i = 0; i <= max; ++i) {
         for (int32_t j = 0; j <= max; ++j) {
-            sum += i;
+            sum += j;
             // to minimize overflow behavior,
             // mask bits
             sum = sum & 0xfff;
