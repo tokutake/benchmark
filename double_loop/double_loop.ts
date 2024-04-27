db768f1a8eb4b290fc44bf02818bee0fc589aceb
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 17:00:31 +0900

    Fix bug
    Use AND operator to cut off upper bits

--- double_loop/double_loop.ts
+++ double_loop/double_loop.ts
@@ -2,10 +2,10 @@ console.log(Deno.args[0]);
 const MAX : number = parseInt(Deno.args[0]);
 
 let num: number = 0;
-for (let i: number = 0; i < MAX; i++) {
-    for (let j: number = 0; j < MAX; j++) {
+for (let i: number = 0; i <= MAX; i++) {
+    for (let j: number = 0; j <= MAX; j++) {
         num += j;
-        num = num ^ 0xfff;
+        num = num & 0xfff;
     }
 }
 
