442261dfc4321f7de8a3b03242cb3711e554cb19
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2024-04-27 15:45:56 +0900

    Add C++ program

---
+++ double_loop/double_loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    int32_t max = std::stoi(argv[1]);
+    int32_t sum = 0;
+
+    for (int32_t i = 0; i <= max; ++i) {
+        for (int32_t j = 0; j <= max; ++j) {
+            sum += i;
+            // to minimize overflow behavior,
+            // mask bits
+            sum = sum & 0xfff;
+        }
+    }
+
+    std::cout << sum << std::endl;
+
+    return 0;
+}
